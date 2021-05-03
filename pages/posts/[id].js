915307151f7806d56f90d8cfbafa17e25ebb7ed8
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-03 18:35:23 +0200

    dynamically params - html content with markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,24 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      { postData.title }
-      <br />
-      { postData.id }
-      <br />
-      { postData.date }
+      <Head>
+        <title>{ postData.title }</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{ postData.title }</h1>
+        {/* { postData.id }
+        <br /> */}
+        <div className={utilStyles.lightText}>
+          <Date dateString={ postData.date } />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
@@ -24,7 +34,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
